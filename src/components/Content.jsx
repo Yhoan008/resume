4b3c3f9a0dd6e0ce8a99c83cca550a14896b0c38
Yhoan008 <yhoanmoreno8@gmail.com>
2024-06-04 20:44:40 -0500

    Se actualiza version

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,9 @@
-import React, { useContext, useEffect, useState } from "react";
-import Mycontext from "./../App";
+import React from "react";
 
-export default function Content({ responsive, children }) {
+export default function Content() {
 
   return (
-    <div className="w-full h-[200vh] pt-[170px] ">
+    <div className="w-full h-[200vh] pt-[10px] ">
       <h1 className="text-black font-['Irish_Grover'] tracking-wider text-2xl text p-3 ">
         DESARROLLADOR Y DISEÑADOR UX
       </h1>
